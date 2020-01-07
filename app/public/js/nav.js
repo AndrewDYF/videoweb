ed899d106ef40777ca5743c5cfbc968dc8380c30
AndrewDYF <duzong007@163.com>
2020-01-07 21:25:08 +0000

    video

---
+++ app/public/js/nav.js
@@ -0,0 +1,20 @@
+const navs = [{
+  title: 'home',
+  icon: '&#xe60c;',
+  spread: false,
+}, {
+  title: 'user manage',
+  icon: '&#xe613;',
+  href: '/user/manager',
+  spread: false,
+}, {
+  title: 'video manage',
+  icon: '&#xe600;',
+  href: '/video/manager',
+  spread: false,
+}, {
+  title: 'comment manage',
+  icon: '&#xe609;',
+  href: '/comment/manager',
+  spread: false,
+}];
